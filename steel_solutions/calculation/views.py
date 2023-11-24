5199bf79c3aa1b3871617e86ac2034d04d5836d0
knyazhnin.sv <knyazhnin.sv@ccnpp.ru>
2023-11-24 11:56:48 +0700

    commit

--- steel_solutions/calculation/views.py
+++ steel_solutions/calculation/views.py
@@ -33,33 +33,7 @@ def sheet_sortament(request):
     return render(request, "calculation/sheet.html", context)
 
 
-def save_sheet_entry(request):
-    form = SheetSpecForm(request.POST)
-    if form.is_valid():
-        instance = SheetSpec.objects.get(id=request.POST.get('id'))
-        instance.unit_type = SheetUnit.objects.get(id=request.POST['unit_type'])
-        instance.width_sheet = request.POST['width_sheet']
-        instance.height_sheet = request.POST['height_sheet']
-        instance.amount = request.POST['amount']
-        instance.save()
-
-
-def save_pogon_entry(request):
-    form = PogonSpecForm(request.POST)
-    if form.is_valid():
-        instance = PogonageSpec.objects.get(id=request.POST.get('id'))
-        instance.unit_type = PogonageUnit.objects.get(id=request.POST['unit_type'])
-        instance.detail_length = request.POST['detail_length']
-        instance.amount = request.POST['amount']
-        instance.save()
-
-
 def spec(request, product_id):
-    if request.method == "POST":
-        if request.POST.get('type') == 'sheet':
-            save_sheet_entry(request)
-        elif request.POST.get('type') == 'pogon':
-            save_pogon_entry(request)
     spec_id = Specification.objects.get(pk=product_id).id
     sheet_spec = SheetSpec.objects.filter(spec_id=spec_id)
     pogonage_spec = PogonageSpec.objects.filter(spec_id=spec_id)
@@ -94,9 +68,31 @@ def create_spec_entry(request):
     if request.method == "POST":
         current_spec = Specification.objects.get(id=request.POST['spec'])
         if request.POST['item_type'] == 'sheet':
-            SheetSpec.objects.create(spec=current_spec, unit_type=SheetUnit.objects.first(), width=0, height=0,
+            SheetSpec.objects.create(spec=current_spec, unit_type=SheetUnit.objects.first(), width_sheet=0, height_sheet=0,
                                      amount=0)
         elif request.POST['item_type'] == 'pogon':
             PogonageSpec.objects.create(spec=current_spec, unit_type=PogonageUnit.objects.first(), detail_length=0,
                                         amount=0)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '<default_url>'))
+
+
+def save_spec_entry(request):
+    if request.method == "POST":
+        if request.POST.get('type') == 'sheet':
+            form = SheetSpecForm(request.POST)
+            if form.is_valid():
+                instance = SheetSpec.objects.get(id=request.POST.get('id'))
+                instance.unit_type = SheetUnit.objects.get(id=request.POST['unit_type'])
+                instance.width_sheet = request.POST['width_sheet']
+                instance.height_sheet = request.POST['height_sheet']
+                instance.amount = request.POST['amount']
+                instance.save()
+        elif request.POST.get('type') == 'pogon':
+            form = PogonSpecForm(request.POST)
+            if form.is_valid():
+                instance = PogonageSpec.objects.get(id=request.POST.get('id'))
+                instance.unit_type = PogonageUnit.objects.get(id=request.POST['unit_type'])
+                instance.detail_length = request.POST['detail_length']
+                instance.amount = request.POST['amount']
+                instance.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '<default_url>'))
