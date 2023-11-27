2f1fb0957d2006f111746920699a35d48e24bc6c
knyazhnin.sv <knyazhnin.sv@ccnpp.ru>
2023-11-27 15:57:37 +0700

    commit

--- steel_solutions/calculation/views.py
+++ steel_solutions/calculation/views.py
@@ -49,17 +49,17 @@ def get_spec(product_id):
 def calculate_sheet_spec_entry(unit_type, width_detail, height_detail, amount):
     unit_num = round(
         decimal.Decimal((width_detail * height_detail) / (unit_type.unit_width * unit_type.unit_height)) * amount, 4)
-    summary_weight = round(
+    weight = round(
         decimal.Decimal((width_detail / 1000 * height_detail / 1000)) * amount * unit_type.unit_weight, 1)
-    summary_cost = round(unit_num * unit_type.unit_cost)
-    return {'num': unit_num, 'weight': summary_weight, 'cost': summary_cost}
+    cost = round(unit_num * unit_type.unit_cost)
+    return {'num': unit_num, 'weight': weight, 'cost': cost}
 
 
 def calculate_pogon_spec_entry(unit_type, length_detail, amount):
     unit_num = round(decimal.Decimal(length_detail / unit_type.unit_length) * amount, 4)
-    summary_weight = round(decimal.Decimal(length_detail / 1000) * amount * unit_type.unit_weight, 1)
-    summary_cost = round(unit_num * unit_type.unit_cost)
-    return {'num': unit_num, 'weight': summary_weight, 'cost': summary_cost}
+    weight = round(decimal.Decimal(length_detail / 1000) * amount * unit_type.unit_weight, 1)
+    cost = round(unit_num * unit_type.unit_cost)
+    return {'num': unit_num, 'weight': weight, 'cost': cost}
 
 
 def product(request, product_id):
